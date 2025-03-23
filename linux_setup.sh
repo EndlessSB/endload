85a384387316a0ac72cf6d7b30f0447bb19f2b3f
endless <endless@endlesssb.com>
2025-03-23 20:52:35 +0000

    Linux Setup Updates

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,7 +1,18 @@
 echo "Endload Setup"
 
+# Install Python and dependencies
+sudo apt install python3.12 python3.12-venv python3.12-pip
+
+# Create a virtual environment
+python3.12 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install required Python modules
 pip install bcrypt pyfiglet termcolor requests flask python-dotenv
 
 echo "Installed Python Modules"
 
-python3 setup.py
+# Run setup.py (if applicable)
+python setup.py
