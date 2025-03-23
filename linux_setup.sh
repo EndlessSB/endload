342210a6d70972619577a4c5b0ac6a0d5ce0211c
endless <endless@endlesssb.com>
2025-03-23 20:52:53 +0000

    Fixing minor issue

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,7 +1,7 @@
 echo "Endload Setup"
 
 # Install Python and dependencies
-sudo apt install python3.12 python3.12-venv python3.12-pip
+sudo apt install python3.12 python3.12-venv 
 
 # Create a virtual environment
 python3.12 -m venv venv
