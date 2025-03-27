c48b610c32b3c8e52e1667b9ee31b5fb005c710f
Endless <endless@endlesssb.com>
2025-03-27 15:55:51 +0000

    Update linux_setup.sh

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,10 +1,18 @@
 echo "Endload Setup"
 
+echo "Downloading From github"
+
+sudo curl -i "https://github.com/EndlessSB/endload/archive/refs/heads/main.zip" > /endload
+
+"Sucsess"
+
+cd /endload
+
 # Install Python and dependencies
 sudo apt install python3.12 python3.12-venv 
 
 # Create a virtual environment
-python3.12 -m venv venv
+python -m venv venv
 
 # Activate the virtual environment
 source venv/bin/activate
